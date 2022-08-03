75594aefc621c046eca50ebee4ef6628adb65714
s1avyan <s1avyan27@gmail.com>
2022-08-03 23:11:43 +0300

    правки

--- js/modal.js
+++ js/modal.js
@@ -14,21 +14,21 @@
     }
   })();
 
-  (() => {
-    const refs = {
-      openMenuBtn: document.querySelector(".mobile-button__open"),
-      closeMenuBtn: document.querySelector(".mobile-button__close"),
-      menu: document.querySelector(".mobile_menu"),
-    };
+  // (() => {
+  //   const refs = {
+  //     openMenuBtn: document.querySelector(".mobile-button__open"),
+  //     closeMenuBtn: document.querySelector(".mobile-button__close"),
+  //     menu: document.querySelector(".mobile_menu"),
+  //   };
   
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  //   refs.openMenuBtn.addEventListener("click", toggleMenu);
+  //   refs.closeMenuBtn.addEventListener("click", toggleMenu);
   
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-      refs.menu.classList.toggle("no-scroll");
-    }
-  })();
+  //   function toggleMenu() {
+  //     refs.menu.classList.toggle("is-hidden");
+  //     refs.menu.classList.toggle("no-scroll");
+  //   }
+  // })();
 
   // (() => {
   //   const refs = {
